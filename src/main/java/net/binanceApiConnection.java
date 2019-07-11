2ce816aeab77fe5efa81f6ab4f239aad76c41f2e
unknown <sebastianrogoz@gmail.com>
2019-07-11 15:15:17 +0200

    initial commit

---
+++ src/main/java/net/binanceApiConnection.java
@@ -0,0 +1,15 @@
+package net;
+
+import java.net.HttpURLConnection;
+import org.json.*;
+
+public class binanceApiConnection {
+    private final String apiKey;
+    private final String secretKey;
+
+    public binanceApiConnection(String apiKey, String secretKey) {
+        this.apiKey = apiKey;
+        this.secretKey = secretKey;
+    }
+
+}

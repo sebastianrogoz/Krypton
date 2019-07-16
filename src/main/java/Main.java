acd403346fb7a6252b8279554bf8b6fd1743f22d
unknown <sebastianrogoz@gmail.com>
2019-07-16 15:56:21 +0200

    16.07end

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,20 @@
 import net.BinanceApiConnection;
+import DAL.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
         BinanceApiConnection exConn = new BinanceApiConnection();
+        DatabaseConnection dbConn = DatabaseConnection.getInstance();
         List<String> symbols = exConn.getSymbolsList();
 
         for(String symbol : symbols) {
             System.out.println(symbol);
         }
+
+        //https://api.binance.com/api/v3/avgPrice?symbol=USDCUSDT
 
     }
 }

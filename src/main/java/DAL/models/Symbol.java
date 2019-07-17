f8c09a16191b76cb4ac99622d4a7ea6991c6f3ce
unknown <sebastianrogoz@gmail.com>
2019-07-17 12:37:27 +0200

    17.06

---
+++ src/main/java/DAL/models/Symbol.java
@@ -0,0 +1,33 @@
+package DAL.models;
+
+public class Symbol {
+    private int id;
+    private String symbol;
+    private double price;
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+}

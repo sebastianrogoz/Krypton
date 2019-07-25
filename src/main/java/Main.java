21d6bec7bdaa4de010765264ef0fbf0383d4f51b
unknown <sebastianrogoz@gmail.com>
2019-07-25 15:49:45 +0200

    25.07

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,8 @@ import NET.BinanceApiConnection;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println(DAL.Repositories.ExchangeRateRepository.getCurrencyBySymbol("XZCBNB").getValue());
-        System.out.println(BinanceApiConnection.getSymbolPrice("XZCBNB"));
+        System.out.println(DAL.Repositories.ExchangeRateRepository.getCurrencyBySymbol("BNBBTC").getValue());
+        System.out.println(BinanceApiConnection.getSymbolPrice("BNBBTC"));
     }
 }
 
